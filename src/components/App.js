129709ab433ac17099a01e4381b42e3681f2be76
Kishanvaghasiya108 <kishanvaghasiya2008@gmail.com>
2023-04-03 18:55:25 +0530

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,23 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
+  const[value,setValue] = useState("");
+  const[final,setFinal] = useState("____");
+
+  function inputHandler(e){
+    setValue(e.target.value);
+  }
+  function clickHandler(){
+    setFinal(value);
+    setValue("");
+  }
 //code here
+
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={inputHandler} value={value}></input>
+      <button id='button' onClick={clickHandler}>Click</button>
+      <p id='text'> Hello my name is {final} and I study at Newton School</p>
     </div>
   )
 }
